687bdcfa3c3271e31e0fb5c84a8985fd21dd1994
any67 <any_sf_1967@hotmail.com>
2017-10-30 19:57:04 -0300

    calculo area de un rectangulo

---
+++ src/pruebajava1/calcularArea.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+/**
+ *
+ * @author LAB08
+ */
+public class calcularArea 
+{
+ //Meodo constructor
+   
+ int base;
+ int altura;
+ int area;
+ 
+ 
+
+
+  public static void main(String[] args)
+  {
+  System.out.println("Ingrese valor base");
+   System.out.println("Ingrese valor altura");
+   System.out.println("calcular=base*altura");
+  
+  }
+          
+
+          
+          
+          }
+
+
+
+
+
+
+
+}
