96e0691954f4ca64a27d8544f2e7e11fa6b9a7fe
any67 <any_sf_1967@hotmail.com>
2017-10-30 20:30:59 -0300

    Esta clase recibe los datos por ventana

---
+++ src/Datospersonales.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB08
+ */
+public class Datospersonales 
+{
+ public static void main(String[] args)
+ {
+ String nombre=("Digite su nombre");
+ String edad=("Digite su edad");
+ System.out.println("B);
+ }        
+         
+}
